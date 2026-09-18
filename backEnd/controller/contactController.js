39842f2272806a4d95abad3647e6f4e9a7fb4d9c
Ranida01 <senmith01@gmail.com>
2026-09-18 22:38:55 +0530

    added the database connection

--- backEnd/controller/contactController.js
+++ backEnd/controller/contactController.js
@@ -1,21 +1,23 @@
+const asyncHandler = require('express-async-handler');
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access Public
-const getContacts = (req, res) => {
+const getContacts =asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Get a single contact
 //@route GET /api/contacts/:id
 //@access Public
-const getContact = (req, res) => {
+const getContact =asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Get contact with ID ${req.params.id}` });
-};
+});
 
 //@desc Create a new contact
 //@route POST /api/contacts
 //@access Public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
     console.log("The request body is: ", req.body);
     const {name,email,phone} = req.body;
     if(!name || !email || !phone){
@@ -23,21 +25,21 @@ const createContact = (req, res) => {
         throw new Error("All fields are mandatory!");
     }
     res.status(201).json({ message: "Create a new contact" });
-};
+});
 
 //@desc Update a contact
 //@route PUT /api/contacts/:id
 //@access Public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update contact with ID ${req.params.id}` });
-};
+});
 
 //@desc Delete a contact
 //@route DELETE /api/contacts/:id
 //@access Public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete contact with ID ${req.params.id}` });
-};
+});
 
 
 module.exports = { getContacts, createContact, getContact, updateContact, deleteContact };
--- backEnd/server.js
+++ backEnd/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const errorHandler = require('./middleware/errorHandler');
+const connectDb = require('./config/dbConnection');
 
+connectDb();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
