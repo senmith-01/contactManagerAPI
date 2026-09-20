8aa5c75fedd10cef62eecc805b0683842e4a2896
Ranida01 <senmith01@gmail.com>
2026-09-21 02:01:35 +0530

    rename 'userModels' to 'userModels' and updated the userController

--- backEnd/controller/userController.js
+++ backEnd/controller/userController.js
@@ -1,10 +1,37 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 //@desc Register a new user
 //@route POST /api/users
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Register the user" });
+    const { username, email, password } = req.body;
+    if(!username || !email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const userAvailable = await User.findOne({ email });
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already registered!");
+    }
+    // hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log('Hashed password:', hashedPassword);
+    const user = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    });
+    console.log('Created user:', user);
+    if(user){
+        res.status(201).json({ _id: user.id, email: user.email });
+    }else{
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
+    res.json({ message: "User registered successfully" });
 });
 
 //@desc Login a user
