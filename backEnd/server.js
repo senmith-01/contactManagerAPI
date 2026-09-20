7391302e2f134dd8556bd05369ae97385cb76717
Ranida01 <senmith01@gmail.com>
2026-09-21 01:05:28 +0530

    Added user routes

--- backEnd/server.js
+++ backEnd/server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require('./routes/contactRoutes'));
+app.use("/api/users", require('./routes/userRoutes'));
 app.use(errorHandler);
 
 app.listen(PORT, () => {
