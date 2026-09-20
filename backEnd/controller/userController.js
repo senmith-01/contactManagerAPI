2bb88d81a2bca8bc0683f6fa9b6918a9ee6c6ee3
Ranida01 <senmith01@gmail.com>
2026-09-21 02:18:07 +0530

    apply the jsonwebtoken to userController

--- backEnd/controller/userController.js
+++ backEnd/controller/userController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
 
@@ -38,7 +39,25 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //@access Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Login the user" });
+    const { email, password } = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const user = await User.findOne({ email });
+    if(user && await bcrypt.compare(password, user.password)){
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            }
+        }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+        res.status(200).json({ accessToken });
+    }else{
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
 });
 
 //@desc Get current user
